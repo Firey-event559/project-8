868bd15c750ae3ec482c7b01662f5719b2dc3032
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-05-24 15:31:11 +0200

    Initial commit

---
+++ documentatie/Documentatie/uneed-it.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 14 mei 2024 om 12:58
+-- Serverversie: 10.4.32-MariaDB
+-- PHP-versie: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uneed-it`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `offertes`
+--
+
+CREATE TABLE `offertes` (
+  `id` int(11) NOT NULL,
+  `voornaam` varchar(100) NOT NULL,
+  `achternaam` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefoonnummer` int(11) NOT NULL,
+  `titelprobleem` varchar(100) NOT NULL,
+  `serienummer` varchar(100) NOT NULL,
+  `probleemreparatie` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `producten`
+--
+
+CREATE TABLE `producten` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(100) NOT NULL,
+  `specificatie` int(11) NOT NULL,
+  `informatie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `offertes`
+--
+ALTER TABLE `offertes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `producten`
+--
+ALTER TABLE `producten`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `offertes`
+--
+ALTER TABLE `offertes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `producten`
+--
+ALTER TABLE `producten`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+
+
+
+</x-layout>
+
+<main id="mainhome">
+<div id="hometext">
+<img src="{{ Vite::asset('resources/assets/cropped-logo UNEED-IT(notext).png') }}">
+    <p><span class="white-text">voor al uw reparaties kunt u terecht bij </span><span class="red-text">Uneed-</span><span  class="blue-text">it</span></p>
+</div>
+</main>
