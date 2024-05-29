562959d9c3b8aff3ab2e399a22635660d9242698
KEVN <troostkevin8@gmail.com>
2024-05-29 14:25:49 +0200

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,16 @@
 <main id="mainhome">
 <img src="{{ Vite::asset('resources/assets/engineer.jpg') }}" width="100%" height="750px">
 </main>
-
+<div class="container px-4">
+  <div class="row gx-5">
+    <div class="col">
+      <div class="p-3">Custom column padding Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, itaque saepe earum suscipit, in qui vero necessitatibus harum facere sapiente eum libero deserunt illum ad molestias sint iste id minus?</div>
+    </div>
+    <div class="col">
+      <div class="p-3"><img src="{{ Vite::asset('resources/assets/background2.png') }}" width="400px" height="300px"   ></div>
+    </div>
+  </div>
+</div>
 
 </x-layout>
 
