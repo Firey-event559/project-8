57987e50e8cf6922cacf76e57253fd87094187aa
KEVN <troostkevin8@gmail.com>
2024-05-29 13:00:32 +0200

    commit homepage

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite (['resources/css/app.css'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
     <title>Document</title>
 </head>
 <body>
@@ -26,7 +31,9 @@
     </div>
 </nav>
 
+{{ $slot }}
 
+<<<<<<< Updated upstream
     {{ $slot }}
 
 <footer id="footer">
@@ -46,8 +53,52 @@
         <p>MA T/M VRIJ, 09:00 - 23:00</p>
         <p>TELEFONISCH BEREIKBAAR</p>
         <p>VOOR ABONNEMENTHOUDERS</p>
+=======
+
+<footer class="bg-body-tertiary text-center text-lg-start" id="footer">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Footer text</h5>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+          voluptatem veniam, est atque cumque eum delectus sint!
+        </p>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Footer text</h5>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+          voluptatem veniam, est atque cumque eum delectus sint!
+        </p>
+      </div>
+      <!--Grid column-->
+>>>>>>> Stashed changes
     </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2020 Copyright:
+    <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
 </footer>
+
+
+
 
 </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,12 @@
 <x-layout>
 
+<<<<<<< Updated upstream
 <h1 class="test">Registration</h1>
+=======
+<main id="mainhome">
+>>>>>>> Stashed changes
 
+</main>
 
 </x-layout>
 
