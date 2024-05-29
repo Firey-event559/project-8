ba876fe1c017b36ab80a0aac72709de8eba9e436
Vasco van Gils <vascovangils@gmail.com>
2024-05-29 13:07:24 +0200

    Services Page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +12,9 @@
         </script>
     <title>Document</title>
 </head>
+
 <body>
+<<<<<<< Updated upstream
 <nav id="navbar">
     <div id="logonav">
     <img src="{{ Vite::asset('resources/assets/cropped-logo UNEED-IT.png') }}">
@@ -30,9 +32,29 @@
         </ul>
     </div>
 </nav>
+=======
+    <nav id="navbar">
+        <div id="logonav">
+            <img src="{{ Vite::asset('resources/assets/cropped-logo UNEED-IT.png') }}">
+        </div>
+        <div id="logoptions">
+            <ul>
+                <li class="redc"> <a href="{{ url('/index')}}">Home</a> </li>
+                <li class="bluec"> <a href="OverOns.html">Over ons </a></li>
+                <li class="redc"> <a href="{{ url('/services') }}">Service </a></li>
+                <li class="bluec"> <a href="zakelijk.html">Zakelijk </a></li>
+                <li class="redc"> <a href="faq.html">Faq </a> </li>
+                <li class="bluec"><a href="Bezorgdiensten.html"> Bezorgdiensten </a></li>
+                <li class="redc"><a href="{{ url('/login_signup') }}">Account</a></li>
+
+            </ul>
+        </div>
+    </nav>
+>>>>>>> Stashed changes
 
 {{ $slot }}
 
+<<<<<<< Updated upstream
 
 <footer class="bg-body-tertiary text-center text-lg-start" id="footer">
   <!-- Grid container -->
@@ -74,9 +96,31 @@
   </div>
   <!-- Copyright -->
 </footer>
+=======
+    <footer id="footer">
+        <div id="adress">
+            <img src="{{ Vite::asset('resources/assets/location.png') }}">
+            <p>ZUIDBAAN 514, 2841MD</p>
+            <p> MOORDRECHT</p>
+        </div>
+        <div id="telefoonnnumer">
+            <img src="{{ Vite::asset('resources/assets/phone.png') }}">
+            <p>+316 30 985 409 SERVICENUMMER</p>
+            <p>+3118 28 202 18 KANTOOR </p>
+            <p> BEREIKBAAR VAN 09:00-18:00</p>
+        </div>
+        <div id="tijd">
+            <img src="{{ Vite::asset('resources/assets/clock.png') }}">
+            <p>MA T/M VRIJ, 09:00 - 23:00</p>
+            <p>TELEFONISCH BEREIKBAAR</p>
+            <p>VOOR ABONNEMENTHOUDERS</p>
+        </div>
+    </footer>
+>>>>>>> Stashed changes
 
 
 
 
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,13 @@ Route::get('/', function () {
 });
 
 Route::get('/login_signup', function () {
-    return view('/login_signup'); 
- });
+    return view('/login_signup');
+});
 
 Route::get('/index', function () {
     return view('/index');
-
 });
+<<<<<<< Updated upstream
 Route::get('/webshop', function () {
     return view('/webshop');
 
@@ -27,4 +27,9 @@ Route::get('/login', function () {
 Route::get('/signup', function () {
     return view('/signup');
 
+=======
+
+Route::get('/services', function () {
+    return view('/services');
+>>>>>>> Stashed changes
 });
