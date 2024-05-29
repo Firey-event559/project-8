aaaa148f9fb412563218b40ebbf19115ab63a598
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-05-29 13:05:11 +0200

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,27 +33,6 @@
 
 {{ $slot }}
 
-<<<<<<< Updated upstream
-    {{ $slot }}
-
-<footer id="footer">
-    <div id="adress">
-        <img src="{{ Vite::asset('resources/assets/location.png') }}">
-        <p>ZUIDBAAN 514, 2841MD</p>
-       <p> MOORDRECHT</p>
-    </div>
-    <div id="telefoonnnumer">
-        <img src="{{ Vite::asset('resources/assets/phone.png') }}">
-        <p>+316 30 985 409 SERVICENUMMER</p>
-        <p>+3118 28 202 18 KANTOOR </p>
-        <p> BEREIKBAAR VAN 09:00-18:00</p>
-    </div>
-    <div id="tijd">
-        <img src="{{ Vite::asset('resources/assets/clock.png') }}">
-        <p>MA T/M VRIJ, 09:00 - 23:00</p>
-        <p>TELEFONISCH BEREIKBAAR</p>
-        <p>VOOR ABONNEMENTHOUDERS</p>
-=======
 
 <footer class="bg-body-tertiary text-center text-lg-start" id="footer">
   <!-- Grid container -->
@@ -83,7 +62,6 @@
         </p>
       </div>
       <!--Grid column-->
->>>>>>> Stashed changes
     </div>
     <!--Grid row-->
   </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,6 @@
 <x-layout>
 
-<<<<<<< Updated upstream
-<h1 class="test">Registration</h1>
-=======
 <main id="mainhome">
->>>>>>> Stashed changes
 
 </main>
 
