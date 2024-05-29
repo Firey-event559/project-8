fb6d45a5a42055262a663a9ba917265b18db862c
KEVN <troostkevin8@gmail.com>
2024-05-29 13:08:08 +0200

    current change

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,7 +14,6 @@
 </head>
 
 <body>
-<<<<<<< Updated upstream
 <nav id="navbar">
     <div id="logonav">
     <img src="{{ Vite::asset('resources/assets/cropped-logo UNEED-IT.png') }}">
@@ -32,29 +31,9 @@
         </ul>
     </div>
 </nav>
-=======
-    <nav id="navbar">
-        <div id="logonav">
-            <img src="{{ Vite::asset('resources/assets/cropped-logo UNEED-IT.png') }}">
-        </div>
-        <div id="logoptions">
-            <ul>
-                <li class="redc"> <a href="{{ url('/index')}}">Home</a> </li>
-                <li class="bluec"> <a href="OverOns.html">Over ons </a></li>
-                <li class="redc"> <a href="{{ url('/services') }}">Service </a></li>
-                <li class="bluec"> <a href="zakelijk.html">Zakelijk </a></li>
-                <li class="redc"> <a href="faq.html">Faq </a> </li>
-                <li class="bluec"><a href="Bezorgdiensten.html"> Bezorgdiensten </a></li>
-                <li class="redc"><a href="{{ url('/login_signup') }}">Account</a></li>
-
-            </ul>
-        </div>
-    </nav>
->>>>>>> Stashed changes
 
 {{ $slot }}
 
-<<<<<<< Updated upstream
 
 <footer class="bg-body-tertiary text-center text-lg-start" id="footer">
   <!-- Grid container -->
@@ -96,27 +75,6 @@
   </div>
   <!-- Copyright -->
 </footer>
-=======
-    <footer id="footer">
-        <div id="adress">
-            <img src="{{ Vite::asset('resources/assets/location.png') }}">
-            <p>ZUIDBAAN 514, 2841MD</p>
-            <p> MOORDRECHT</p>
-        </div>
-        <div id="telefoonnnumer">
-            <img src="{{ Vite::asset('resources/assets/phone.png') }}">
-            <p>+316 30 985 409 SERVICENUMMER</p>
-            <p>+3118 28 202 18 KANTOOR </p>
-            <p> BEREIKBAAR VAN 09:00-18:00</p>
-        </div>
-        <div id="tijd">
-            <img src="{{ Vite::asset('resources/assets/clock.png') }}">
-            <p>MA T/M VRIJ, 09:00 - 23:00</p>
-            <p>TELEFONISCH BEREIKBAAR</p>
-            <p>VOOR ABONNEMENTHOUDERS</p>
-        </div>
-    </footer>
->>>>>>> Stashed changes
 
 
 
