1cf2c006481cd9dd04bb556fea8e004faf745838
Aaron24115 <9018817@student.zadkine.nl>
2024-05-31 10:39:09 +0200

    update login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,10 +3,9 @@
     <div class="form-container">
         <h2 class="form-title">Login</h2>
         <form id="registration-form" action="auth.php" method="post">
-            <input type="text" class="" name="Naam" id="Naam" placeholder="Naam"><br>
             <input type="text" class="" name="email" id="email" placeholder="Email"><br>
-            <input type="password" class="" name="password" id="password" placeholder="Password"><br>
-            <button type="submit" >Log in</button>
+            <input type="password" class="" name="wachtwoord" id="password" placeholder="wachtwoord"><br>
+            <input type="submit" name= "inloggen">
         </form>
     </div>
 </main>
--- resources/views/login_signup.blade.php
+++ resources/views/login_signup.blade.php
@@ -1,9 +1,11 @@
 <x-layout>
-    <div class="container">
-        <div class="login-signup">
-            <h1>Het lijkt erop dat jij niet ingelogd is</h1>
-            <button onclick="window.location.href='{{ url('/login') }}'">Log In</button>
-            <button onclick="window.location.href='{{ url('/signup') }}'">Sign Up</button>
-        </div>
+<div class="container">
+    <div class="login-signup">
+        <h1>Het lijkt erop dat jij niet ingelogd is</h1>
+        <button onclick="window.location.href='{{ url('/login') }}'">Log In</button>
+        <button onclick="window.location.href='{{ url('/signup') }}'">Sign Up</button>
+    </div>
     </div>
 </x-layout>
+
+
