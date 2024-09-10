33569d51ce3d8e78ec7b8b952fe5486014b77eda
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-09-10 11:16:24 +0200

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <?php 
 
-$email = Session::get('user_email');
+
 
 ?>
 
@@ -13,7 +13,7 @@ $email = Session::get('user_email');
   <div class="row gx-5">
     <div class="col">
     @auth
-        <p>Ingelogd als: {{ $email }}</p>
+        <p>Ingelogd als: {{ Auth::user()->email; }}</p>
     @endauth
      <div class="p-3">Welkom bij Uneed-it, uw vertrouwde partner voor al uw reparatiebehoeften. Bij Uneed-it streven we ernaar hoogwaardige kwalitatieve reparatiediensten te bieden met een onwrikbare focus op klanttevredenheid en excellentie in kwaliteit. Met een schat aan ervaring in de sector hebben we een onberispelijke reputatie opgebouwd als een bedrijf dat synoniem staat voor vakmanschap, snelle service en eerlijke prijzen. Of het nu gaat om het herstellen van elektronica, huishoudelijke apparaten, auto's of andere technische apparaten, ons team van deskundige technici staat paraat om uw problemen op te lossen en uw apparaten weer in optimale staat te herstellen.</div>
     </div>
