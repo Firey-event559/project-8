4f0007e5982b1e1b769d4ae1940ba30ad350a0c7
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-09-12 13:16:58 +0200

    update admindashboard

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,8 +1,18 @@
 <x-layout>
 
-   @auth
+    @auth
     <p>Ingelogd als: {{ Auth::user()->email; }}</p>
-    @endauth    
+    @endauth
+
+
+    <h1 class="admin_dashboard_text">Admin Dashboard</h1>
+
+    <div class="button_admin_container">
+        <button class="producten_insert">Producten Toevoegen</button>
+        <button class="offerte_search">Offertes inzien </button>
+        <button class="producten_update">Producten Bewerken</button>
+        <button class="producten_list">Producten lijst</button>
+    </div>
 
 
 </x-layout>
