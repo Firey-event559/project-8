6df1c5756cc4932803a5e3e71b78e15a80bc5b04
Vasco van Gils <vascovangils@gmail.com>
2024-09-13 09:55:42 +0200

    Registratie Succes Pagina Fix
    Nu heeft de Registratie succes pagina geen grote hoeveelheid aan wit in de pagina

--- resources/views/signup_succes.blade.php
+++ resources/views/signup_succes.blade.php
@@ -2,8 +2,8 @@
 
  <div class="container_signup">
 
-    <h1 class="signup">Registratie succesvol</h1>
-    <p class="signup">Uw registratie is succesvol verlopen. U kunt nu inloggen.</p>
+    <h1 class="signup_success">Registratie succesvol</h1>
+    <p class="signup_success">Uw registratie is succesvol verlopen. U kunt nu inloggen.</p>
     <a class="signup button" href="{{url('/login')}}">Inloggen</a>
 
     </div>
