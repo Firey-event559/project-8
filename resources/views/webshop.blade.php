b07b2396f36a9af16b32ccbb56abba2d901f574e
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-09-18 14:29:22 +0200

    update

--- resources/views/webshop.blade.php
+++ resources/views/webshop.blade.php
@@ -1,14 +1,14 @@
 <x-layout>
 
 
-   @foreach ($products as $product)
+   
+<h1 class="Product_webshop_tekst">Producten </h1>
+@foreach ($products as $product)
 
     <div class="Product_webshop">
-     <img src="{{ $product->image }}" class="card-img-top" alt="foto product" with="100px" height="100px" >
-     <div class="">
-        <h5 class="card-title">{{ $product->name }}</h5>
-        <p class="card-text">{{ $product->description }}</p>
-        <p class="card-text">{{ $product->price }}</p>
+    <img src="{{ $product->Image }}" class="Image" alt="foto product" style="width: 100px; height: 100px;"> 
+     <h5 class="card-title">{{ $product->Name }}</h5>
+        <p class="card-text">€ {{ $product->Price }}</p>
      </div>
 
 
