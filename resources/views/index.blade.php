f8433032220cd4ae718c820292604be502b7ce07
KEVN <troostkevin8@gmail.com>
2024-09-18 14:02:44 +0200

    homepage revamp

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,25 @@
 <x-layout>
 
 <main id="mainhome">
-<img src="{{ Vite::asset('resources/assets/engineer2.jpg') }}" width="100%" height="">
+<div
+    class="p-5 text-center bg-image"
+    style="
+      background-image: url('/recources/assets/banner.png') ;
+      height: 400px;
+    "
+  >
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">Heading</h1>
+          <h4 class="mb-3">Subheading</h4>
+          <a data-mdb-ripple-init class="btn btn-outline-light btn-lg" href="#!" role="button"
+          >Call to action</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 <div class="container overflow-hidden text-center">
   <div class="row gx-5">
