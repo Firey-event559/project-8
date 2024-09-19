8ad5f669d5ecca48ce50589d5687f05acfb128cf
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-09-19 14:46:27 +0200

    update
    update

--- resources/views/admin_offerte.blade.php
+++ resources/views/admin_offerte.blade.php
@@ -6,7 +6,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite (['resources/css/app.css'])
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -51,6 +50,8 @@
                         <td>{{ $offerte->phonenumber }}</td>
                         <td>{{ $offerte->serialnumber }}</td>
                         <td>{{ $offerte->details }}</td>
+                        <td><input type="submit" value="verwijderen"> </td>
+                        <td><input type="submit" value="voltooien"> </td>
                     </tr>
                     @endforeach
 
