73859297b0052a98f9b5f31cfe4c76b0181951b4
KEVN <troostkevin8@gmail.com>
2024-09-20 12:27:54 +0200

    homepage changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,21 +17,35 @@
       </div>
     </div>
   </main>
-  <div class="container overflow-hidden text-center">
-    <div class="row gx-5">
-      <div class="col">
-        <p class="p-3">Welkom bij Uneed-it, uw vertrouwde partner voor al uw reparatiebehoeften. Bij Uneed-it streven
+  <div class="container text-center p-5">
+    
+    <div class="col-md-6 text-start">
+      <h4>De beste hulp voor u.</h4>
+      <h4 class="subheading">skibidi gyatt rizzler</h4>
+      <p class="text-muted">Send and receive files. Deliver digital assets in a secure environment.
+        Share feedback in real time. Use GetLance Messages to communicate via text, chat, or video.
+        Use our mobile app. Many features can be accessed on your mobile phone when on the go.</p>
+    </div>
+    <div class="col-md-6 ">
+      <img src="{{ Vite::asset('resources/assets/winkel.jpg') }}" width="450px" height="300px" >
+    </div>
+  </div>
+
+
+  <div class="container text-center p-5">
+    <div class="col-md-6 how-img">
+      <img src="{{ Vite::asset('resources/assets/engineer.jpg') }}" width="450px" height="300px">
+    </div>
+    <div class="col-md-6 text-start">
+      <h2>Over ons</h2>
+      <h4 class="subheading">10 jaar ervaring in de reparatiediensten.</h4>
+      <p class="text-muted">Welkom bij Uneed-it, uw vertrouwde partner voor al uw reparatiebehoeften. Bij Uneed-it streven
           we ernaar hoogwaardige kwalitatieve reparatiediensten te bieden met een onwrikbare focus op klanttevredenheid
           en excellentie in kwaliteit. Met een schat aan ervaring in de sector hebben we een onberispelijke reputatie
           opgebouwd als een bedrijf dat synoniem staat voor vakmanschap, snelle service en eerlijke prijzen. Of het nu
           gaat om het herstellen van elektronica, huishoudelijke apparaten, auto's of andere technische apparaten, ons
           team van deskundige technici staat paraat om uw problemen op te lossen en uw apparaten weer in optimale staat
           te herstellen.</p>
-      </div>
-      <div class="col">
-        <div class="p-3"><img src="{{ Vite::asset('resources/assets/background2.png') }}" width="400px" height="300px">
-        </div>
-      </div>
     </div>
   </div>
 
