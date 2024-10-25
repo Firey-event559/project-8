6a02aa3aca427388b7d7c7b1a2c9df9f0503b583
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-10-25 14:36:15 +0200

    update css fix admin_change

--- resources/views/admin_change.blade.php
+++ resources/views/admin_change.blade.php
@@ -1,25 +1,16 @@
 <x-layout>
-
-    <!DOCTYPE html>
-    <html lang="en">
-
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite (['resources/css/app.css'])
+        @vite(['resources/css/app.css'])
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-            </script>
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </head>
 
-
-
     <body class="admin_change">
-
         <div class="sidebar">
-
             <a href="{{ url('/admin') }}">Producten toevoegen</a>
             <a href="{{ url('/admin_offerte') }}">Offertes inzien </a>
             <a href="{{ url('/admin_change') }}">Producten bewerken</a>
@@ -28,7 +19,7 @@
             <a href="{{ url('/admin_it-nieuws-verwijder') }}">IT Nieuws verwijderen</a>
         </div>
 
-        <div class="content1">
+        <div class="content9">
             <h2>Producten bewerken</h2>
 
             @if(session('success'))
@@ -46,7 +37,7 @@
                             <p class="product_price">€ {{ $product->Price }}</p>
                             <p>{{ $product->Description }}</p>
 
-                            <form action="{{ route('edit', $product->id) }}" method="Get">
+                            <form action="{{ route('edit', $product->id) }}" method="GET">
                                 @csrf
                                 <input type="hidden" name="id" value="{{ $product->id }}">
                                 <input class="edit btn btn-primary" type="submit" value="Bewerken">
@@ -56,13 +47,12 @@
                                 method="POST" style="display: inline;">
                                 @csrf
                                 @method('DELETE')
-                                <input class="winkelmand" type="submit" value="Verwijderen">
+                                <input class="winkelmand btn btn-danger" type="submit" value="Verwijderen">
                             </form>
                         </div>
                     </div>
                 </div>
             @endforeach
         </div>
-
-    </html>
+    </body>
 </x-layout>
