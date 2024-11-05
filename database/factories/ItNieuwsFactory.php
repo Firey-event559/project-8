e88a6228686f6b99e877fb64bc0888cd68c62220
Aaron24115 <9018817@student.zadkine.nl>
2024-11-05 15:29:47 +0100

    update

--- database/factories/ItNieuwsFactory.php
+++ database/factories/ItNieuwsFactory.php
@@ -3,21 +3,33 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\it_nieuws;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\it_nieuws>
  */
 class ItNieuwsFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = it_nieuws::class;
     public function definition(): array
     {
+        $images = [
+            'images/Seeder.png',
+        ];
+        $titles = [
+            'voorbeeld 1',
+            'voorbeeld 2',
+            'voorbeeld 3',
+            ];
+
         return [
-            //
+            'title' => $this->faker->randomElement($titles),
+            'description' => $this->faker->sentence,
+            'Image' => $this->faker->randomElement($images),
+            'created_at' => now(), 
+            'updated_at' => now(),
         ];
     }
 }
+
+
